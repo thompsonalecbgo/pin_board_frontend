26bf566f6004abe338f0cb021d56c8b0558783e3
Thompson Go <thompsongo2020@gmail.com>
2020-12-12 09:33:56 +0800

    checking why notes is not saving edits in localstorage

--- components/editable-note.js
+++ components/editable-note.js
@@ -49,6 +49,7 @@ export default function EditableNote({ note }) {
 
   const handleBlur = useCallback(
     (e) => {
+      console.log("notes edited")
       e.stopPropagation();
       setEditing(false);
       setNotes(
